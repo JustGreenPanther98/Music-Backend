f8cf8b584f7b3f1c7d70f49e77320bffb46d4605
Vedant Maheshwari <vedantmaheshwari2006@gmail.com>
2026-01-23 00:00:04 +0530

    fix pom.xml

--- src/main/java/com/songs/wallah/service/implementation/UserServiceImpl.java
+++ src/main/java/com/songs/wallah/service/implementation/UserServiceImpl.java
@@ -58,6 +58,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public UserDTO getUser(String email) {
 		
+		
 		ModelMapper modelMapper = new ModelMapper();
 		UserEntity userEntity = userRepository.findByEmail(email);
 		if(userEntity==null) {
